26b7ac6b801d67ade87e242622355bb694a855fb
Art <abc@abc.ru>
2018-07-07 00:12:29 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
+
+class App extends Component {
+  render() {
+    return (
+	   <div className="wrapper">
+	     <SayFullName name="Artem" surname="Prygin" link="https://fb.com" />
+	     <SayFullName name="Lena" surname="Lenina" link="https://vk.com" />
+	     <SayFullName name="Ivan" surname="Ivanov" link="https://tickets.fifa.com/productList" />
+	   </div>
+    );
+  }
+}
+
+function SayFullName(props) {
+  return (
+    <div>
+      <h1> Моё имя {props.name}, фамилия {props.surname} </h1>
+      <a href={props.link}>Ссылка на мой профиль </a>
+    </div>
+    )
+}
+
+
+export default App;
